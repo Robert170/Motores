63277fdc17e4030d09681e391036b53f838058d3
DESKTOP-5HJ107M\Xc170 <xc1700@hotmail.com>
2021-02-05 00:57:05 -0700

    constructors de plane

--- xcUtilities/include/xcPlane.h
+++ xcUtilities/include/xcPlane.h
@@ -1,8 +1,37 @@
 #pragma once
 
+#include "xcPlatformTypes.h"
+#include "xcVectorI4.h"
+
+using xcEngineSDK::int32;
+
 class Plane {
  public: 
   Plane() = default;
   ~Plane() = default;
+
+  Plane(int32 x, int32 y = 0, int32 z = 0, int32 d = 0)
+        :m_x(x), m_y(y), m_z(z), m_d(d) { };
+
+  Plane(VectorI2 v, int32 z = 0, int32 d = 0)
+        :m_x(v.m_x), m_y(v.m_y), m_z(z), m_d(d) { };
+
+  Plane(VectorI3 v, int32 d = 0)
+        :m_x(v.m_x), m_y(v.m_y), m_z(v.m_z), m_d(d) { };
+
+  Plane(VectorI4 v)
+        :m_x(v.m_x), m_y(v.m_y), m_z(v.m_z), m_d(v.m_w) { };
+
+  Plane()
+        :m_x(0.f), m_y(0.f), m_z(0.f), m_d(0.f) { };
+
+  ~Plane() = default;
+
+ private:
+
+   int32 m_x;
+   int32 m_y;
+   int32 m_z;
+   int32 m_d;
 
 };
